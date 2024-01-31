7de8b65ab84a7028b2705367099795354c0e0937
Ashish Chauhan <144068765+ashishchauhan2004@users.noreply.github.com>
2024-01-31 18:09:08 +0530

    Add files via upload

---
+++ ashish.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var h1Element = document.querySelector('.h1');
+
+
+    h1Element.addEventListener('click', function() {
+        h1Element.style.backgroundColor = 'yellow';
+    });
+});
